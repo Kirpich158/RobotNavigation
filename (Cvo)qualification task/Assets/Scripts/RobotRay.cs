6eb8a47c1532b918f8add6f26642efb73be7b766
Kirill <44980707+Kirpich158@users.noreply.github.com>
2021-09-29 10:28:55 +0200

    Project added
    Added the whole project

---
+++ (Cvo)qualification task/Assets/Scripts/RobotRay.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotRay : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if (Physics.Raycast(transform.position, transform.forward, 0.6f))
+        {
+            transform.Translate(Vector3.zero);
+            // Dead end scenario
+            if (Physics.Raycast(transform.position, -transform.right, 0.6f) 
+                && Physics.Raycast(transform.position, transform.right, 0.8f))
+            {
+                StartCoroutine(Rotation(180f));
+            }
+            // Blocked from right side scenario
+            if (Physics.Raycast(transform.position, -transform.right, 0.6f))
+            {
+                StartCoroutine(Rotation(90f));
+            }
+            // Blocked from left side scenario
+            if (Physics.Raycast(transform.position, transform.right, 0.6f))
+            {
+                StartCoroutine(Rotation(-90f));
+            }
+            if(!Physics.Raycast(transform.position, -transform.right, 0.6f) 
+                && !Physics.Raycast(transform.position, transform.right, 0.6f))
+            {
+                StartCoroutine(Rotation(90f));
+            }
+        }
+    }
+
+    IEnumerator Rotation(float y)
+    {
+        transform.Rotate(0f, y, 0f);
+        yield return new WaitForSecondsRealtime(1f);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
